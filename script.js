3029950b3d831aca51b97a6fb9eeba9c52065cf3
kristinabog <kristinabog7@hotmail.com>
2021-01-29 16:58:12 +0000

    added flipCard function

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+// variables
+
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+
+// Event Listeners
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
+// Functions
+
+// Flipping the cards 
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if(!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second card
+        hasFlippedCard = false;
+        secondCard = this;
+    }
+}
 
