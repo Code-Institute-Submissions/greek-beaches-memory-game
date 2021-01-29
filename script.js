f9febc137fa8af0b10c73f0faacbd34218136d7f
kristinabog <kristinabog7@hotmail.com>
2021-01-29 17:19:06 +0000

    added functions checkForMatch, disableCards and unFlipCards

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
-
-
 // Event Listeners
 
 cards.forEach(card => card.addEventListener('click', flipCard))
@@ -25,6 +23,31 @@ function flipCard() {
         // second card
         hasFlippedCard = false;
         secondCard = this;
+
+        checkForMatch();
     }
 }
 
+//Do the cards/data-numbers match?
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.number === secondCard.dataset.number;
+    
+    isMatch ? disableCards() : unFlipCards();
+}
+
+// Cards match -> remove click eventListener so they are not clickable
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+// Cards do not match -> remove flip class so they flip back
+
+function unFlipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
